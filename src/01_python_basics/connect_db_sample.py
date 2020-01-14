615d7e9eacda59c2a489b30536c2431013489566
QuanBang <Quan.Bang@stepstone.co.uk>
2020-01-14 11:50:12 +0000

    Add errorhandling

--- src/01_python_basics/connect_db_sample.py
+++ src/01_python_basics/connect_db_sample.py
@@ -1,30 +1,40 @@
 import psycopg2
 
-# create a connection to the redshift db
-con = psycopg2.connect(dbname='mercury_dev'
-                    , user='katauser'
-                    , password='Kata1234'
-                    , host='dev-cluster.cnxohrjczoor.eu-west-1.redshift.amazonaws.com'
-                    , port='5439')
+con = None
 
-# get a cursor from the db connection
-cur = con.cursor()
+try:
 
-# construct sql query
-sqlquery = "select first_name, last_name, age from public.user_details;"
+    # create a connection to the redshift db
+    con = psycopg2.connect(dbname='enigmadb'
+                        , user='katauser'
+                        , password='Kata1234'
+                        , host='enigma-dev-cluster.cnxohrjczoor.eu-west-1.redshift.amazonaws.com'
+                        , port='5439')                    
 
-# execute the query
-cur.execute(sqlquery)
+    # get a cursor from the db connection
+    cur = con.cursor()
 
-# put the results into a list variable
-rows = cur.fetchall()
+    # construct sql query
+    sqlquery = "select first_name, last_name, age from public.user_details;"
 
-# loop through the columns
-for row in rows:
-    print("{0} {1} is {2} years old.".format(row[0],row[1],row[2]))
+    # execute the query
+    cur.execute(sqlquery)
 
-# close cursor
-cur.close()
+    # put the results into a list variable
+    rows = cur.fetchall()
 
-# close connection
-con.close()
+    # loop through the columns
+    for row in rows:
+        print("{0} {1} is {2} years old.".format(row[0],row[1],row[2]))
+
+    # close cursor
+    cur.close()
+
+except (Exception, psycopg2.Error) as err:
+    print(err)
+
+finally:
+
+    if con is not None:
+        # close connection
+        con.close()
