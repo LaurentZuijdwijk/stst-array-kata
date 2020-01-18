fc430a434fbb926eec231e907a6913c416ae55e0
QuanBang <Quan.Bang@stepstone.co.uk>
2020-01-18 15:03:36 +0000

    Update try_except_finally.py

--- src/01_python_basics/try_except_finally.py
+++ src/01_python_basics/try_except_finally.py
@@ -1,20 +1,17 @@
 """
 Python has built-in exception handling. It does so through
 the try-except-finally structure.
-
-You can also raise new exceptions with the "raise" keyword.
 """
 
 try:
     print("This code runs fine until an exception is raised.")
-    raise Exception("This throws an exception.")
-
-    print("This code will not be reachable.")
 except:
     print("This code will execute when an exception is raised in the ""try"" section.")
 finally:
     print("In any case, the ""finally"" block will execute.")
 
+
+# You can raise new exceptions with the "raise" keyword.
 # You can extract the values off from an exception by typing
 # the except block.
 
