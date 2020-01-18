c0e7d79333d33a1214a714f6d442efbe0303233d
QuanBang <Quan.Bang@stepstone.co.uk>
2020-01-18 14:39:33 +0000

    Added tuple example

--- src/01_python_basics/for_loop.py
+++ src/01_python_basics/for_loop.py
@@ -1,9 +1,9 @@
-'''
+"""
 For loops enable your code to iterate over an enumeration
 of values and extract these into a scoped variable.
 
-For loops are useful to iterate over sets or lists.
-'''
+For loops are useful to iterate over sets, tuples or lists.
+"""
 
 # For loops on lists run sequentially.
 
@@ -11,8 +11,15 @@ myList = ["Spain", "France", "United Kingdom", "Narnia"]
 print("Countries I've visited:")
 for country in myList:
     print("- {}".format(country))
+    
+# For loops on tuples run sequentially.
 
-# However, for loops on sets run on no particular order.
+mytuple = ("blue", "green", "yellow", "orange")
+print("Colours of the rainbow:")
+for country in mytuple:
+    print("- {}".format(country))
+
+# However, for loops on sets run in no particular order.
 # Careful with that!
 
 mySet = {"Spain", "France", "United Kingdom", "Narnia"}
