a6c5b09aebdd933ba702de16086ad497faf974cd
QuanBang <Quan.Bang@stepstone.co.uk>
2020-01-18 13:34:41 +0000

    Added comments

--- src/01_python_basics/comments.py
+++ src/01_python_basics/comments.py
@@ -1,28 +1,32 @@
 # This is a single-line comment.
 
-'''
+"""
 This is a multiline comment.
-'''
+"""
 
 # You can concatenate as many single-line comments as you want
-# but you have to prepend every line with the # symbol.
+# but you have to prepend every line with the # symbol. This can be
+# done easily by selecting your code and hitting ctrl key + "/" key.
 
 # Single-line comments are useful as contextual annotations
 # to your code as you type.
 
-'''
+"""
 Multiline comments are useful to document any part of your code
 but you will generally see them documenting the parameters of
-module functions.
+module functions called docstrings. 
+Running the following command: help(yourfunction)
+will display the docstrings of your functions so be aware that
+only the comments you want gets displayed.
 
 Like any other piece of Python code, comments are scoped by
 tabulation.
 
 For example, this is a root-level comment.
-'''
+"""
 
 def say_hello(name):
-    '''
+    """
     This function says hello to the person's name passed as
     parameter.
 
@@ -32,7 +36,7 @@ def say_hello(name):
 
     Parameters:
         name: The name of the person to say hello to.
-    '''
+    """
     print("Hello, {}.".format(name))
 
 say_hello("Little Grasshopper")
