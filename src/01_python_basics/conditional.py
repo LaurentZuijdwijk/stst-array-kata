ee9d2fdd538aef6aa3650ba8704c601a82504213
Quan Bang <Quan.Bang@stepstone.co.uk>
2020-11-20 14:40:49 +0100

    Added one more example.

--- src/01_python_basics/conditional.py
+++ src/01_python_basics/conditional.py
@@ -5,9 +5,22 @@ They are defined with an "if" expression and body, and may also have an
 "else" body. Both "if" and "else" expressions end with a colon (:).
 """
 
+# Example 1
 if(1 == 1):
     print("One is equal to one.")
 else:
     print("This code will evidently never run")
 
 print(":: conditional.py :: DONE\n\n")
+
+
+# Example 2
+x = 5
+y = 10
+if x == y:
+    print("x is equal to y")
+elif x > y:
+    print("x is greater than y")
+else:
+    print("x is less than y")
+
